dc8d518ee824e2ce3c3db2aafbfebd1860efe154
Raja Reddy <raja@cogcons.com>
2018-06-18 13:15:59 +0530

    final commit

---
+++ cogcons-utils/src/main/java/com/cogcons/cogcons/cogcons_utils/CogconsUtils.java
@@ -0,0 +1,14 @@
+package com.cogcons.cogcons.cogcons_utils;
+
+import android.util.Log;
+
+public class CogconsUtils {
+
+    private static final String TAG = "CogconsUtils";
+
+    public static void d(String message) {
+        Log.d(TAG, message);
+    }
+
+
+}
